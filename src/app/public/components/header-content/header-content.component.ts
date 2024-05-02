a1ff9f86074c60a55d46948ff2c9ff967a7209e1
aksoonie <u202210778@upc.edu.pe>
2024-05-02 15:47:32 -0500

    adding icon-page JimenaCama

--- src/app/public/components/header-content/header-content.component.ts
+++ src/app/public/components/header-content/header-content.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import {MatTab, MatTabGroup, MatTabLabel} from "@angular/material/tabs";
-import {PatientsCardsComponent} from "../../../users/components/patients-cards/patients-cards.component";
-import {PsychologistCardsComponent} from "../../../users/components/psychologist-cards/psychologist-cards.component";
+import { PsychologistCardsComponent} from "../../../searchAndMatch/components/psychologist-cards/psychologist-cards.component";
+import {PatientsCardsComponent} from "../../../searchAndMatch/components/patients-cards/patients-cards.component";
 
 @Component({
   selector: 'app-header-content',
   standalone: true,
-    imports: [
-        MatTab,
-        MatTabGroup,
-        MatTabLabel,
-        PatientsCardsComponent,
-        PsychologistCardsComponent
-    ],
+    imports: [MatTab, MatTabGroup, MatTabLabel, PatientsCardsComponent, PsychologistCardsComponent],
   templateUrl: './header-content.component.html',
   styleUrl: './header-content.component.css'
 })
